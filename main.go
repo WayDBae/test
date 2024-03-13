f1ff65447956f6d9f91050129d79c2a54840733d
Daler H. Khayrakov <DalerKh@HUMO.local>
2024-03-13 14:26:13 +0500

    json added.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"dockedis/entities"
+	"encoding/json"
 	"fmt"
 
 	"github.com/go-redis/redis/v8"
@@ -9,7 +11,7 @@ import (
 
 func main() {
 	fmt.Println("Go Redis!")
-
+	ctx := context.Background()
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
@@ -17,21 +19,41 @@ func main() {
 	})
 
 	//TESTING CONNECTION...
-	pong, err := client.Ping(context.Background()).Result()
+	pong, err := client.Ping(ctx).Result()
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(pong)
 	//ДОБАВЛЕНИЕ ЗНАЧЕНИЯ В СЛОВАРЬ NAME
-	err = client.Set(context.Background(), "name", "Daler", 0).Err()
+	err = client.Set(ctx, "name", "Daler", 0).Err()
 	if err != nil {
 		fmt.Println(err)
 	}
 	//ЗАБИРАЕМ ИЗ СЛОВАРЯ NAME ЗНАЧЕНИЕ
-	value, err := client.Get(context.Background(), "name").Result()
+	value, err := client.Get(ctx, "name").Result()
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(value)
+
+	json, err := json.Marshal(entities.User{
+		Name: "Daler",
+		Age:  1,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(json)
+
+	err = client.Set(ctx, "id1234", json, 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	val, err := client.Set(ctx, "id1234", json, 0).Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(val)
 
 }
